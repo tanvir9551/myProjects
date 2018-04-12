8935a38966861e09544b5b368ba86b592ffcced2
Sheikh Tanveer Ahmed <sktanvir0@gmail.com>
2018-04-12 09:52:12 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+	    <?php include'header.php';?>
+
+		<title>Trade Net Tech</title>
+	</head>
+	<body>
+		<div class = "fade-in one">
+			<?php include'nav.php';?>
+		</div>			
+		<div class="solution-grid fade-in two">
+			<h1 align = "center">TRADE <font color = "#236476">NET</font> TECH</h1>
+			<p>"One stop solutions"</p>
+			<br><br><br><br><br><br><br>
+		
+						
+			<div class="grids" data-aos = "fade-up" data-aos-duration = "2000">
+				<h3>About Us</h3>
+				<p>
+					Trade Net Tech (TNT) established in 2014 to fulfill all requirements of Business needs.
+					TNT is a leading Trade Agent to sale & supplies all kinds of IT equipments, accessories
+					and consumable products, Information Technology Solutions, Business and Office
+					Solutions, Production Materials and Data Center Systems, Fire, Safety, Security,
+					Automations, Software Development, Electrical and Electronics, Office Stationeries,
+					Garments, life style, Import and Export. Trade Net Tech provides resourceful, cost-
+					effective and scalable IT solutions to businesses. Our main objective is to build long-term
+					strategic partnership with our valued clients based on loyalty, excellence and faith. I feel
+					sure that our extensive portfolio of services and wide ranging experience could be of
+					assistance to you.
+				</p>
+				<br><br>
+				<h3>Company Strategy</h3>
+				<ul>
+					<li>Purpose: To be a leader in the business by providing enhanced services, relationship and profitability.<br><br></li>
+					<li>Vision: To provide excellence services that makes maximum trust of our esteemed customers.<br><br></li>
+					<li>Mission: Statement to build long term relationships with our customers and clients and provide excellent customer services and support by pursuing business through innovation and advanced technology.<br><br></li>
+					<li>Core values: We believe in treating our customers with respect and faith; We grow through creativity, development and novelty; We integrate sincerity, integrity and business ethics into all aspects of our business functioning.<br><br></li>
+					<li>Goals: To build good reputation in all fields and become a key player in the industry.<br><br></li>
+				</ul>
+				<div>
+					<h3>Our Services</h3>
+					<div class = "photoHolder" >
+						<ul>
+							<li class = "homePhotos" data-aos="flip-left">
+								<img src="images/laptops.png">
+							</li>
+							<li class = "homePhotos" data-aos="flip-right">
+								<img src="images/itc.png">
+							</li>
+							<li class = "homePhotos" data-aos="flip-left">
+								<img src="images/software.png">
+							</li>
+							<li class = "homePhotos" data-aos="flip-right" style = "background: white;">
+								<div class = "showAll"><p>SHOW ALL<br>&rarr;</p></div>
+							</li>
+						</ul>
+					</div>
+				</div>
+				<div class="clear"> </div>
+				<br><br>
+				<div>
+					<h3>Our Valuable Clients</h3>
+					<div class = "photoHolder" >
+						<ul>
+							<li class = "homePhotos" data-aos="fade-left">
+								<img src="images/ucb.png">
+							</li>
+							<li class = "homePhotos" data-aos="fade-right">
+								<img src="images/ansar-vdp.png">
+							</li>
+							<li class = "homePhotos" data-aos="fade-left">
+								<img src="images/deshbandhu.png">
+							</li>
+							<li class = "homePhotos" data-aos="fade-right" style = "background: white;">
+								<div class = "showAll"><p>SHOW ALL<br>&rarr;</p></div>
+							</li>
+						</ul>
+					</div>
+				</div>
+				<div class="clear"> </div>
+			</div>
+						
+		<div class="footer">
+				<div class="wrap">
+					<div class="box1">
+						<h4>Summary</h4>
+						<p>We shall deliver our best support for any business requirements for your company. Look forward to receiving your further instructions for enlistment. However should you have any queries or wish to discuss matters further please do not hesitate to contact us.</p>
+					</div>
+					<div class="box1">
+						<h4>Contact</h4>
+						<ul>
+							<li><p>Location: House # 23,  Road # 08, Level-7 South Baridhara (DIT Project), Merul Badda, Dhaka-1212</p></li>
+						</ul>
+						<ul>
+							<li><p>Email: support@tradenettech.com </p></li>
+						</ul>
+						<ul>
+							<li><p>Mobile: 01717263608(24x7), 01752776566 (Support)</p></li>
+						</ul>
+					</div>
+				</div>
+			<div class="clear"> </div>
+			
+			<?php include'footer.php';?>
+			<div class = "clear"></div>
+		</div>
+		</div>
+		<script src='plugins/aos/aos.js'></script>
+		<script >AOS.init({
+		  duration: 1000,
+		})
+		</script>
+	
+	</body>
+</html>
+
